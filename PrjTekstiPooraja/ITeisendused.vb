85cd30b1bd620d3a0cf8c17c119e611b0dc348a1
Harold Otsus <haotsu@ttu.ee>
2023-02-08 11:43:34 +0200

    Liides ITeisendused valmis. Liidesele ITeisendused lisatud omadused. Liidesele ITeisendused lisatud meetodid.

---
+++ PrjTekstiPooraja/ITeisendused.vb
@@ -0,0 +1,8 @@
+﻿Public Interface ITeisendused
+    Property intAlgus As Integer    'teksti esimene symbol ASCII koodi jaoks
+    Property intLopp As Integer     'teksti viimane symbol ASCII koodi jaoks
+    Property strTekst As String     'kasutaja sisestatud teksti jaoks
+
+    Function pooraTekst() As String     'liidese meetodid koodi
+    Sub teisendaTekst(ByRef strSisendTekst As String)       'ei oma
+End Interface
