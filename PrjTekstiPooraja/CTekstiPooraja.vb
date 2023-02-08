77afc4a6372f1c6804c73c02813ef004aa5f7484
Harold Otsus <haotsu@ttu.ee>
2023-02-08 12:25:00 +0200

    Klassi CTekstiPooraja liidese atribuudid ära kirjeldatud.

--- PrjTekstiPooraja/CTekstiPooraja.vb
+++ PrjTekstiPooraja/CTekstiPooraja.vb
@@ -6,38 +6,49 @@
     Private intLoppSymbol As Integer
     Private strPooratavTekst As String
 
+    'Liidese omaduste (atribuutide) seadistamine
+
+    'Atribuut intAlgus seadistamine
     Private Property intAlgus As Integer Implements ITeisendused.intAlgus
         Get
-            Throw New NotImplementedException()
+            Return intAlgusSymbol       'atribuudi k6simisel tagasta intAlgusSymbol
         End Get
         Set(value As Integer)
-            Throw New NotImplementedException()
+            intAlgusSymbol = value      'atribuudi v22rtuse seadistamine
         End Set
     End Property
 
+    'Atribuut intLopp seadistamine
     Private Property intLopp As Integer Implements ITeisendused.intLopp
         Get
-            Throw New NotImplementedException()
+            Return intLoppSymbol        'atribuudi k6simiselt tagasta intLoppSymbol
         End Get
         Set(value As Integer)
-            Throw New NotImplementedException()
+            intLoppSymbol = value       'atribuudi v22rtuse seadistamine
         End Set
     End Property
 
+    'Atribuut strTekst seadistamine
     Private Property strTekst As String Implements ITeisendused.strTekst
         Get
-            Throw New NotImplementedException()
+            Return strPooratavTekst     'atribuudi küsimisel tagasta strPooratavTekst
         End Get
         Set(value As String)
-            Throw New NotImplementedException()
+            strPooratavTekst = value    'atribuudi v22rtuse seadistamine
         End Set
     End Property
 
-    Private Sub teisendaTekst(ByRef strSisendTekst As String) Implements ITeisendused.teisendaTekst
-        Throw New NotImplementedException()
+    'Meetod(protseduur), mis p66rab tagurpidi sisendparameetrina strSisendTekst
+    'antud avaldise ja salvestab v22rtuse atribuuti strPooratavTekst
+    Private Sub teisendaTekst(ByRef strSisendTekst As String) _
+        Implements ITeisendused.teisendaTekst
+        strPooratavTekst = StrReverse(strSisendTekst)
     End Sub
 
-    Private Function pooraTekst() As String Implements ITeisendused.pooraTekst
-        Throw New NotImplementedException()
+    'Meetod(funktsioon), mis v6tab atribuudist strSisendTekst v22rtuse
+    'ja p66rab selle stringi tagurpidi
+    Private Function pooraTekst() As String _
+        Implements ITeisendused.pooraTekst
+        Return StrReverse(strPooratavTekst)
     End Function
 End Class
