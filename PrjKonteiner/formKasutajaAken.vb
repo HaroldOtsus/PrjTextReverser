d2fe9b584de45ca2eafcb48780ceb3a89679c172
Harold Otsus <haotsu@ttu.ee>
2023-02-08 13:03:48 +0200

    Lisatud nupule btnPoora2  sündmus teisendaTekst().

--- PrjKonteiner/formKasutajaAken.vb
+++ PrjKonteiner/formKasutajaAken.vb
@@ -13,4 +13,19 @@
         'txtValjundTekst1
         txtValjundTekst1.Text = pooraja.pooraTekst()
     End Sub
+
+    Private Sub btnPoora2_Click(sender As Object, e As EventArgs) Handles btnPoora2.Click
+        'Objekti loomine liidese baasil; referentsmuutuja pooraja deklaratsioon
+        Dim pooraja As PrjTekstiPooraja.ITeisendused
+        'Seadistame referentsmuutuja viitama uuele objektile
+        pooraja = New PrjTekstiPooraja.CTekstiPooraja
+
+        'Teostame teksti p66ramise kasutades parametriseeritud protseduuri teisendatekst()
+        'ning liidese atribuuti strTekst
+        'kutsume v2lja komponendis sisalduva protseduuri,
+        'anname talle parameetriks tekstiv2lja v22rtuse
+        pooraja.teisendaTekst(txtSisendTekst.Text)
+        'omistame tekstiv2lja txtValjudTekst1 v22rtuseks liidese atribuudi v22rtuse
+        txtValjundTekst2.Text = pooraja.strTekst
+    End Sub
 End Class
