afb6afa090e8e6e64cb8f401ada601871ebe198d
HaroldOtsus <haroldotsus@gmail.com>
2023-02-11 20:14:19 +0200

    Lisatud komponent CAlgoritmilinePooraja, mis realiseerib liidest ITeisendused.

---
+++ PrjTekstiPooraja/CAlgoritmilinePooraja.vb
@@ -0,0 +1,42 @@
+﻿Public Class CAlgoritmilinePooraja
+    Implements ITeisendused
+
+    Public Property intAlgus As Integer Implements ITeisendused.intAlgus
+        Get
+            Throw New NotImplementedException()
+        End Get
+        Set(value As Integer)
+            Throw New NotImplementedException()
+        End Set
+    End Property
+
+    Public Property intLopp As Integer Implements ITeisendused.intLopp
+        Get
+            Throw New NotImplementedException()
+        End Get
+        Set(value As Integer)
+            Throw New NotImplementedException()
+        End Set
+    End Property
+
+    Public Property strTekst As String Implements ITeisendused.strTekst
+        Get
+            Throw New NotImplementedException()
+        End Get
+        Set(value As String)
+            Throw New NotImplementedException()
+        End Set
+    End Property
+
+    Public Sub teisendaTekst(ByRef strSisendTekst As String) Implements ITeisendused.teisendaTekst
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Function pooraTekst() As String Implements ITeisendused.pooraTekst
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function loeT2ish22likud(strSisendTekst As String) As Integer Implements ITeisendused.loeT2ish22likud
+        Throw New NotImplementedException()
+    End Function
+End Class
